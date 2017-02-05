2728bd00fa8ab36d52b717827175b479369715ac
akca10 <j.jose.souza7@gmail.com>
2017-02-05 15:26:37 -0200

    Update desktop.js

--- desktop.js
+++ desktop.js
@@ -3,12 +3,12 @@ var box;
 var adsVideo;
 var qtdCliques = 0;
 var intervalClique1;
-var segundos = 7;
+var segundos = 10;
 var loading;
 var p1;
 var p2;
 var botao;
-var tempoadblock = 45;
+var tempoadblock = 50;
 
 if (recuperaCookie("clique"))
 {
